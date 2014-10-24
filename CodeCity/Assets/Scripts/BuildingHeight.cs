5d219ef09ce2468b9435117683e8a17c2b027ebc
Cindy Lee <anacindy@live.ca>
2014-10-24 12:03:16 -0700

    Unity: instantiating building objects
    - instantiating building objects
    - replacing & resizing the plane
    - replacing main camera

--- CodeCity/Assets/Scripts/BuildingHeight.cs
+++ CodeCity/Assets/Scripts/BuildingHeight.cs
@@ -2,26 +2,43 @@
 using System.Collections;
 
 public class BuildingHeight : MonoBehaviour {
-	public GameObject building01;
-	public GameObject building02;
-	//public GameObject capsule;
+	public GameObject plane;
+	public GameObject mainCamera;
+
+	int numBuildings = 4;
+	float[] bases = new float[] {0, 1, 2, 0};
+	float[] heights = new float[] {10f, 5f, 4f, 12f};
+	float[,] positions = new float[,] {{0,0}, {3,1}, {5,3}, {8,2}};
 	
 	void Start () {
-		// Used example from : http://docs.unity3d.com/ScriptReference/Transform-localScale.html
-		float height01 = 2f;
-		float height02 = 5f;
+		float planeX = 0;
+		float planeY = 0;
+		float planeZ = 0;
 
-		// increase the height of the buildings
-		building01.transform.localScale += new Vector3 (0, height01, 0);
-		building02.transform.localScale += new Vector3 (0, height02, 0);
+		// Instantiate example from : http://docs.unity3d.com/Manual/InstantiatingPrefabs.html
+		for (int x = 0; x < numBuildings; x++) {
+			GameObject building = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
-		// reposition the buildings to place them on top of the plane
-		building01.transform.position += new Vector3 (-3, height01/2f - 0.5f, 0);
-		building02.transform.position += new Vector3 (0, height02/2f - 0.5f, 2f);
+			building.transform.position = new Vector3(positions[x,0], heights[x]/2 + 0.5f, positions[x,1]);
+			building.transform.localScale += new Vector3 (bases[x], heights[x], bases[x]);
 
-		// duplicate objects
-		//GameObject clone;
-		//clone = Instantiate (building01, transform.position, transform.rotation) as GameObject;
+			if (planeX < positions[x,0]){
+				planeX = positions[x,0];
+			}
+			if (planeY < heights[x]){
+				planeY = heights[x];
+			}
+			if (planeZ < positions[x,1]){
+				planeZ = positions[x,1];
+			}
+		}
+
+		// resize and replace plane to cover all the buildings
+		plane.transform.localScale = new Vector3(planeX/4, 0, planeZ/4);
+		plane.transform.position = new Vector3(planeX/2, 0, planeZ/2);
+
+		// place main camera to show all the buildings
+		mainCamera.transform.position = new Vector3 (planeX / 2, planeY / 2, -14);
 	}
 
 	void Update () {	
