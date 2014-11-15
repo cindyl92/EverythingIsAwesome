c612a0680e4503f9426bbd81c2636671f61d4b7b
whitecaps <white.caps@yahoo.com>
2014-11-15 12:02:58 -0800

    added getArrayLOC and getCommentDensity.
    refactored createLOCJSON functionality into constructor then added array
    getters. merge back into master after JSONParser/Combiner calls to
    CustomParser resolved.

--- CodeCity/Assets/Scripts/CustomParser.cs
+++ CodeCity/Assets/Scripts/CustomParser.cs
@@ -1,46 +1,30 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using UnityEngine;
 
 namespace AssemblyCSharpvs 
 {
-	public class CustomParser :MonoBehaviour //Debug: need MonoBehaviour so that class can be added to a Unity object as a component
+	public class CustomParser
 	{
-		//Debug: need filePath as public so "C:\Users\Kevin\Desktop\textTest20.txt" (no quotes) can be passed to Unity GUI
-		//Change to private in production
-		public string filePath;
 		private int commentCount;
 		private int locCounter;
-
-		//constructor
+		private int commentDensity;
+		private ArrayList arrayCommentDensity = new ArrayList();
+		private ArrayList arrayLOC = new ArrayList();
+		
+		//constructor: creates arrayCommentDensity and arrayLOC which will be accessed by JSONParser/Combiner 
 		public CustomParser(string filePath) {	
-			this.filePath = filePath;	
-		}
-
-		//Debug only
-		public int getCommentCount(){
-			return commentCount;
-		}
-
-		//commentDensity = (commentCount/(commentCount + LOC)) * 100
-		//safeLOC lets this getCommentDensity execute without having to first call createLOCJSON.
-		//Refactor: Move createLOCJSON to constructor then create getLOC(). Bruce can then just use getLOC() and getCommentDensity()
-		public int getCommentDensity(){
-			int safeLOC = this.createLOCJSON ();
-			return (int) Math.Round (((double)commentCount)/(commentCount+safeLOC)*100);
-		}
-
-		public int createLOCJSON(){
-
+			
 			string stringProbe;
+			string breakSequence = "@#$";
 			bool inLineComment = false;
 			bool inBlockComment = false;
-
+			
 			//StreamReader implementation inspired by: http://msdn.microsoft.com/en-us/library/system.io.streamreader.readline%28v=vs.110%29.aspx
-
+			
 			try
 			{
 				using (StreamReader sr = new StreamReader(filePath))
@@ -50,39 +34,58 @@ namespace AssemblyCSharpvs
 						stringProbe = sr.ReadLine(); //reads in the next line from text
 						stringProbe = stringProbe.Trim(); //removes all leading and trailing white space from a line
 						
+						//if we reach the end of the page, reset all flags and counters, and add commentDensity and LOC to arrays
+						if (stringProbe == breakSequence){
+							inLineComment = false;
+							inBlockComment = false;
+							
+							//calculation: commentDensity = (# of comments)/(# of comments + LOC) * 100
+							commentDensity = (int) Math.Round (((double)commentCount)/(commentCount+locCounter)*100);
+							arrayCommentDensity.Add(commentDensity);
+							arrayLOC.Add(locCounter);
+							
+							locCounter = 0;
+							commentCount = 0;
+							
+							if (sr.Peek () >= 0){
+								stringProbe = sr.ReadLine();
+								stringProbe = stringProbe.Trim();
+							}
+							
+						}
+						
 						if (stringProbe.Length > 1){ //this will ignore lines of code that only contain one '}'
-
+							
 							//if the line being read is the end of a block comment, then set inBlockComment to false
 							if ((stringProbe[(stringProbe.Length)-2] == '*') && (stringProbe[(stringProbe.Length)-1] == '/'))
 							{
 								inBlockComment = false;
 								locCounter--;
-
+								
 							}
-
+							
 							//if line being read is the start of a line comment, then set inLineComment to true
 							if ((stringProbe[0] == '/') && (stringProbe[1] == '/'))
 							{
 								inLineComment = true;
 							}
-
+							
 							//if line being read is the start of a block comment, then increment commentCount
 							if ((stringProbe[0] == '/') && (stringProbe[1] == '*'))
 							{
 								commentCount++;
-
+								
 								//if block comment is only 1 line long, then set inBlockComment to false right away, else set to true
 								if ((stringProbe[(stringProbe.Length)-2] == '*') && (stringProbe[(stringProbe.Length)-1] == '/'))
 								{
 									inBlockComment = false;
-
+									
 								}else{
 									inBlockComment = true;
 								}
-
+								
 							}
-
-
+							
 							//if line being read is neither an inline comment nor in a block comment, then increment locCounter,
 							//else increment commentCount
 							if ((!inLineComment) && (!inBlockComment))
@@ -104,19 +107,30 @@ namespace AssemblyCSharpvs
 			{
 				Console.WriteLine("The process failed: {0}", e.ToString());
 			}
-
+			
+		} //end of constructor
+		
+		public ArrayList getArrayLOC(){
+			return arrayLOC;
+		}
+		
+		public ArrayList getArrayCommentDensity(){
+			return arrayCommentDensity;	
+		}
+		
+		//Debug only.
+		public int getCommentCount(){
+			return commentCount;
+		}
+		
+		//Debug only.
+		public int getLOC(){
 			return locCounter;
 		}
-			
-		void Start(){
-
-			//string mockFilePath = @"C:\Users\Kevin\Desktop\textTest20.txt";
-			//CustomParser cusPar = new CustomParser(mockFilePath);
-			//Console.WriteLine (cusPar.createLOCJSON());
-			Debug.Log ("LOC = " + this.createLOCJSON ());
-			Debug.Log ("Comment Count = " + commentCount);
-			Debug.Log ("commentDensity = " + this.getCommentDensity ());;
-			
+		
+		//Debug only.
+		public int getCommentDensity(){
+			return commentDensity;
 		}
 	}
 }
