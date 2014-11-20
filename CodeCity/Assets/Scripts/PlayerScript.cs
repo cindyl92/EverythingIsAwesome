5524efee9fca349be0928504f19bde0fa418021d
whitecaps <white.caps@yahoo.com>
2014-11-19 16:59:26 -0800

    added camera navigation and bldg labels

---
+++ CodeCity/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	public float speed;
+	public Camera lowCam;
+	public Camera highCam;
+	// Use this for initialization
+	void Start () {
+		lowCam.enabled = true; 
+		highCam.enabled = false; 
+	
+	}
+	
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			lowCam.enabled = !lowCam.enabled;
+			highCam.enabled = !highCam.enabled;
+		}
+		
+	}
+}
